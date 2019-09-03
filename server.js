975fd5b4a7d2bd0d2f9a9aa4e55587ee7ab534e9
Jason O'Dell <jeodell@ncsu.edu>
2019-09-03 18:01:06 -0400

    Changing redirect uri to use gh pages

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/callback', function(req, res) {
   }
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    let uri = 'https://jeodell.github.io/SpotifyNewReleases/' || 'http://localhost:3000' // process.env.FRONTEND_URI
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
